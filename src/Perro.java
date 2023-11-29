8afa5bc76c23700f2f00edfd6049b9dac13c402f
ivanblancosantamaria <ivan.blanco.movil@gmail.com>
2023-11-29 20:24:20 +0100

    A falta del main

--- src/Perro.java
+++ src/Perro.java
@@ -1,2 +1,22 @@
 public class Perro {
+
+
+    Boolean amigable=false;
+
+    public Boolean getAmigable() {
+        return amigable;
+    }
+
+    public void setAmigable(Boolean amigable) {
+        this.amigable = amigable;
+    }
+
+    @Override
+    public String toString() {
+        return "Perro{" +
+                "amigable=" + amigable +
+                '}';
+    }
+
+
 }
